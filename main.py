353cac8d9ffeb64906b2e7a1f73cf5a58f6a585a
Miguel Pacheco <miguelpacheco@usc02gp21tq05n.lan>
2024-01-18 21:18:05 -0600

    print product ID on line 48

--- main.py
+++ main.py
@@ -2,42 +2,50 @@ import requests
 import json
 import creds
 
+
 def getCategoryIds(server, account):
-     # build out URI
-    url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/brightpearl-category/" % (server, account)
-    
-     # add headers
+    # build out URI
+    url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/brightpearl-category/" % (
+        server, account)
+
+    # add headers
     headers = {
         "content-type": "application/json",
         "brightpearl-app-ref": creds.app_ref,
         "brightpearl-account-token": creds.api_key
     }
-    
+
     response = requests.get(url=url, headers=headers).json()
-    
+
     data = response.get("response")
-    
+
     for d in data:
         catId = d["id"]
         catName = d["name"]
-        print(f"The category ID for {catName} is {catId}")
-    
+        print(f"category ID for {catName} is: {catId}")
+
+
 getCategoryIds("use1", "mpachecotest")
+
 
 def updateProducts(server, account):
     # user Inputs
-    userCategory = input("Using the above ^^^ type in the ID of the CATEGORY you want to assign products to: ")
-    userProducts = input("Which products do you want to assign to this category?")
+    userCategory = input(
+        "Using the above ^^^ type in the ID of the CATEGORY you want to assign products to: ")
+    userProducts = input(
+        "Which products do you want to assign to this category?")
     # build out URI
-    url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/product/%s" % (server, account, userProducts)
-    
+    url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/product/%s" % (
+        server, account, userProducts)
+
     headers = {
         "content-type": "application/json",
         "brightpearl-app-ref": creds.app_ref,
         "brightpearl-account-token": creds.api_key
     }
-    
+
     response = requests.get(url=url, headers=headers).json()
-    print(response["response"][0])
-    
+    print(response["response"][0]["id"])
+
+
 updateProducts("use1", "mpachecotest")
