38b8884d427b9e906dd88a6306b55021766f0c05
Miguel Pacheco <miguelwebdev765@gmail.com>
2024-01-18 22:12:33 -0600

    add notes and spacing to format. add logic to print success message based on returned response code

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import json
 import creds
 
+
+# function to run GET to retrieve categories
+
 def getCategoryIds(server, account):
      # build out URI
     url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/brightpearl-category/" % (server, account)
@@ -15,6 +18,7 @@ def getCategoryIds(server, account):
     
     response = requests.get(url=url, headers=headers).json()
     
+    #print all category names and ids to user
     data = response.get("response")
     
     for d in data:
@@ -24,8 +28,11 @@ def getCategoryIds(server, account):
     
 getCategoryIds("use1", "mpachecotest")
 
+# function to run PUT request based on user input
+
 def updateProducts(server, account):
     # user Inputs
+    
     userCategory = input("Using the above ^^^ type in the ID of the CATEGORY you want to assign products to: ")
     body = {
         "reporting": {
@@ -34,6 +41,7 @@ def updateProducts(server, account):
     }
     userProducts = input("Which products do you want to assign to this category?")
     # build out URI
+    
     url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/product/%s" % (server, account, userProducts)
     
     headers = {
@@ -42,7 +50,13 @@ def updateProducts(server, account):
         "brightpearl-account-token": creds.api_key
     }
     
+    # complete PUT request and prints out message to user
+    
     response = requests.put(url=url, headers=headers, json=body)
-    print(response)
+    response = response.status_code
+    if response == 200:
+        print(f"Successfully updated sub category to ID: {userCategory}!!!!")
+    else:
+        print(f"Something went wrong {response}")
     
 updateProducts("use1", "mpachecotest")
