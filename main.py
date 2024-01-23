cca27ca333e9b58e2df81767968ffed9e0c4f3e3
Miguel Pacheco <miguelwebdev765@gmail.com>
2024-01-22 21:50:06 -0600

    create variable to store parent category ID based on user input. create a second body dictionary called data to be sent depending on if a parent category exists or not. add if statement to check which request to be sent, depending on a parent cateogry ID existing or not.

--- main.py
+++ main.py
@@ -30,17 +30,29 @@ def getCategoryIds(server, account):
         catParents[catId] = catParent
         print(f"The category ID for {catName} is {catId}")
         
-    print(catParents)
     
     # user Inputs
     
-    userCategory = input("Using the above ^^^ type in the ID of the CATEGORY you want to assign products to: ")
+    userCategory = int(input("Using the above ^^^ type in the ID of the CATEGORY you want to assign products to: "))
+    
+    # store parent category based off user input in dictionary to use for body2
+    
+    userCatParent = int(catParents.get(userCategory, None))
+    
     body = {
         "reporting": {
-        "subcategoryId": userCategory
+            "subcategoryId": userCategory
+		}
+    }
+    
+    data = {
+        "reporting": {
+            "categoryId": userCatParent,
+            "subcategoryId": userCategory
 		}
     }
     userProducts = input("Which products do you want to assign to this category?")
+   
     # build out URI
     
     url = "https://%s.brightpearlconnect.com/public-api/%s/product-service/product/%s" % (server, account, userProducts)
@@ -53,7 +65,21 @@ def getCategoryIds(server, account):
     
     # complete PUT request and prints out message to user
     
-    response = requests.put(url=url, headers=headers, json=body)
+    if userCatParent == 0:
+        response = requests.put(url=url, headers=headers, json=body)
+        # print("Request body:", json.dumps(body, indent=2))
+    elif userCatParent != 0:
+        # print("Request body:", json.dumps(data, indent=2))
+        response = requests.put(url=url, headers=headers, json=data)
+    else:
+        print("Something went wrong!!")
+   
+    # print request that is being sent
+        
+    # print("Request URL:", url)
+    # print("Request Headers:", headers)
+    # print("Request Body:", json.dumps(data, indent=2))
+    
     response = response.status_code
     if response == 200:
         print(f"Successfully updated sub category to ID: {userCategory}!!!!")
